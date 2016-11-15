5ee9629ab0533421181d840b13ed207873dc28b6
shimenyimeng <xuefeigu@163.com>
2016-11-15 15:33:28 +0800

    本地推送通知
    涉及到category的action

--- 032-- iOS 10 远程推送通知/AppDelegate.m
+++ 032-- iOS 10 远程推送通知/AppDelegate.m
@@ -7,8 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import <UserNotifications/UserNotifications.h>
 
-@interface AppDelegate ()
+@interface AppDelegate () <UNUserNotificationCenterDelegate>
 
 @end
 
@@ -16,10 +17,80 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    // 获取center
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    // 设置回调
+    center.delegate = self;
+    
+    // 请求用户授权
+    [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert) completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        
+        if (granted) {
+            
+            NSLog(@"用户允许推送通知");
+        } else {
+            
+            NSLog(@"用户不允许");
+        }
+        
+    }];
+    
+    // 先注册category
+    // 给center设置category
+    // 还需要给content设置categoryIdentifier
+    UNNotificationAction *textInputAction = [UNTextInputNotificationAction actionWithIdentifier:@"textInput" title:@"文本输入" options:UNNotificationActionOptionForeground textInputButtonTitle:@"说吧" textInputPlaceholder:@"说点什么吧"];
+    UNNotificationAction *foregroundAction = [UNNotificationAction actionWithIdentifier:@"foreground" title:@"前台" options:UNNotificationActionOptionForeground];
+    UNNotificationAction *destructiveAction = [UNNotificationAction actionWithIdentifier:@"destructive" title:@"毁灭" options:UNNotificationActionOptionDestructive];
+    // category
+    UNNotificationCategory *category = [UNNotificationCategory categoryWithIdentifier:@"吃饭" actions:@[foregroundAction, destructiveAction, textInputAction] intentIdentifiers:@[@"destructive", @"foreground"] options:UNNotificationCategoryOptionCustomDismissAction];
+    // 赋值
+    [center setNotificationCategories:[NSSet setWithObject:category]];
+    
     return YES;
 }
 
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    
+    NSLog(@"%@", deviceToken);
+    
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+    
+    // 可以展示通知之前在这里设置通知
+    completionHandler(UNNotificationPresentationOptionSound | UNNotificationPresentationOptionAlert);
+    
+}
+
+// 用户点击了通知后调用
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)())completionHandler {
+    
+    // 通过 userInfo 的内容来决定页面跳转或者是进行其他操作
+    NSDictionary *userInfo = response.notification.request.content.userInfo;
+    NSLog(@"%@", userInfo);
+    // 直接告诉系统你已经完成了所有工作
+    
+#pragma mark - 关于对action的处理，也是通过这里的判断
+    NSString *categoryIdentifier = response.notification.request.content.categoryIdentifier;
+    
+    // 判断categoryIdentifier
+    if ([categoryIdentifier isEqualToString:@"吃饭"]) {
+        
+        NSString *actionIdentifier = response.actionIdentifier;
+        
+        // 判断actionIdentifier
+        if ([actionIdentifier isEqualToString:@"textInput"]) {
+            
+            // 如果用户输入了文本并点击了按钮，才会走到这里
+            
+        }
+        
+    }
+    
+    completionHandler();
+    
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- 032-- iOS 10 远程推送通知/ViewController.m
+++ 032-- iOS 10 远程推送通知/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <UserNotifications/UserNotifications.h>
 
 @interface ViewController ()
 
@@ -16,14 +17,47 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor redColor];
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+# pragma mark - 还是本地推送!!!!!!!!!!!!!!!!!!!!!!!
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    // 创建content
+    UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
+    content.title = @"内地和粉底";
+    content.body = @"京东看佛教恩对方立刻开纠纷";
+    content.sound = [UNNotificationSound defaultSound];
+    content.userInfo = @{@"name" : @"guxuefei", @"age" : @20};
+    
+    // 设置categoryIdentifier，用于输入文字、关闭、进入等
+    content.categoryIdentifier = @"吃饭";
+    
+    UNTimeIntervalNotificationTrigger *trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:5.0 repeats:NO];
+    // 创建远程推送通知请求，标识符可以用来管理通知，可以取消、更新、移除通知
+    // 这些管理，都只是针对本地推送，远程推送还不能这样做
+    UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:@"好吧" content:content trigger:trigger];
+    [[UNUserNotificationCenter currentNotificationCenter] addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+        
+        NSLog(@"推送成功");
+        
+    }];
+    
+    // 移除还未展示的通知
+//    [[UNUserNotificationCenter currentNotificationCenter] removePendingNotificationRequestsWithIdentifiers:@[@"好吧"]];
+    
+    // 移除已经展示的通知
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(7.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        
+//        [[UNUserNotificationCenter currentNotificationCenter] removeDeliveredNotificationsWithIdentifiers:@[@"好吧"]];
+//    });
+    // 更新通知，不管有没有已经展示，都可以进行更新，只需要传入相同identifiers的新的请求
+    // 多次推送同一标识符的通知即可进行更新
+    
+    
 }
-
 
 @end
